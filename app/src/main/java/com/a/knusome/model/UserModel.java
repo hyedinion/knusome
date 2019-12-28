9b82f62b153908009af91f412b73bafe30620114
hyedinion <ja03129@naver.com>
2019-12-28 23:54:05 +0900

    결과

---
+++ app/src/main/java/com/a/knusome/model/UserModel.java
@@ -0,0 +1,13 @@
+package com.a.knusome.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class UserModel {
+    public String uid;
+    public String userName;
+    public String userAge;
+    public String profileImageUrl;
+    public String dept;
+    public List<String> blockid = new ArrayList<>();
+}
